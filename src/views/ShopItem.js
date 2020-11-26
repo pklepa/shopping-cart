2363cdbfd4044fb5d288dc29a3de02e85cde9c14
Pedro Klepa <pedro.klepa@gmail.com>
2020-11-26 12:35:17 +0000

    Add option to proceed to checkout straight from shopItem

--- src/views/ShopItem.js
+++ src/views/ShopItem.js
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Catalog from "../assets/catalog.json";
 
 function ShopItem({ itemId, addItemToCart }) {
+  const [showCheckoutBtn, setShowCheckoutBtn] = useState(false);
+
   const item = Catalog.find((i) => i.id == itemId);
+
+  function handleClick() {
+    addItemToCart({ ...item, quantity: 1 });
+    setShowCheckoutBtn(true);
+  }
 
   return (
     <div>
       <h1>You clicked on {item.name}</h1>
 
-      <button onClick={() => addItemToCart({ ...item, quantity: 1 })}>
-        Add to cart
-      </button>
+      <button onClick={handleClick}>Add to cart</button>
+
+      {showCheckoutBtn && (
+        <button>
+          <Link to="/checkout">Proceed to checkout</Link>
+        </button>
+      )}
 
       <Link to="/shop">Go back</Link>
     </div>
